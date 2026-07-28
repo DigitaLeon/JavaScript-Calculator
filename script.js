2484c26431a30c44421a531fe729e3bdde1a0c43
DigitaLeon <Saijin41@gmail.com>
2026-07-28 17:13:23 -0400

    Add initial calculator project

---
+++ script.js
@@ -0,0 +1,45 @@
+const display = document.getElementById('display');
+const displayVal = document.getElementById('display').value;
+const btnButton = document.querySelector('.btn');
+
+function appendToDisplay(val){
+    display.value += val;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function deleteLast(){
+    display.value = display.value.substring(0, display.value.length - 1);
+}
+
+function calculateResult(){
+    let result = eval(display.value);
+    display.value = result;
+    console.log(result);
+}
+
+
+//Keyboard event handling
+document.addEventListener('keydown', (event) =>{
+   if(event.key === "Backspace"){
+        deleteLast();
+    }
+    
+    if(event.key === 'Enter'){
+        calculateResult();
+    }
+    
+    if(!isNaN(event.key)){
+        appendToDisplay(event.key);
+    }
+    
+    if("+-*/.".includes(event.key)){
+        appendToDisplay(event.key);
+    }
+    
+    if(event.key === "Delete"){
+        clearDisplay();
+    }
+});
